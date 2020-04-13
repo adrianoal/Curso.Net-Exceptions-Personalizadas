7980c15c7e44379c78c1b43d08f3af55f8a9a736
Adriano de Assis Lima <adriano.lima85@yahoo.com.br>
2020-04-13 19:14:23 -0300

    Aula 144 - Tratamento de Exception Personalizadas

---
+++ ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ExcecoesPersonalizadas.Entities;
+using ExcecoesPersonalizadas.Entities.Exceptions;
+
+namespace ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number:");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("check-out (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+
+
+                Console.WriteLine("Enter data  to the reservation: ");
+                Console.Write("check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("check-out (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e) //Exception Generico
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
+
